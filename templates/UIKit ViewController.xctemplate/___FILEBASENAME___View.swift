72202f5a3d6f63fe53c9f5e7bf967fce4102085f
Andy Nadal <andy@nadalapps.com>
2022-01-27 10:02:25 -0600

    First commit

---
+++ templates/UIKit ViewController.xctemplate/___FILEBASENAME___View.swift
@@ -0,0 +1,21 @@
+//___FILEHEADER___
+//
+// Template by Andy Nadal
+// Github: @andynadal
+
+import UIKit
+
+final class ___VARIABLE_productName:identifier___View: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureConstraints()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    private func configureConstraints() {
+        
+    }
+}
---
+++ templates/UIKit ViewController.xctemplate/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,18 @@
+// ___FILEHEADER___
+//
+// Template by Andy Nadal
+// Github: @andynadal
+
+import UIKit
+
+final class ___VARIABLE_productName:identifier___ViewController: UIViewController {
+    var customView = ___VARIABLE_productName:identifier___View()
+
+    override func loadView() {
+        view = customView
+    }    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

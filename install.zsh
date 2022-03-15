bdeba6ab8a8c747ce4bb5fb508eed06bbf8f0a9b
Andy Nadal <andy@nadalapps.com>
2022-03-15 06:38:37 -0600

    Changed install script to zsh

--- install.zsh
+++ install.zsh
@@ -1,5 +1,8 @@
+#!/usr/bin/env zsh
+
 echo "1. Installing code snippets"
 
+mkdir ~/Library/Developer/Xcode/UserData/CodeSnippets/ 
 if cp ./snippets/* ~/Library/Developer/Xcode/UserData/CodeSnippets/ ; then
     echo "     Successfuly installed code snippets"
 else
@@ -10,6 +13,7 @@ fi
 
 echo "2. Installing file templates"
 
+mkdir /Applications/Xcode.app/Contents/Developer/Library/Xcode/Templates/File\ Templates/User\ Interface/
 if cp -r ./templates/interface/* /Applications/Xcode.app/Contents/Developer/Library/Xcode/Templates/File\ Templates/User\ Interface/ ; then
     echo "     Successfully installed templates"
 else

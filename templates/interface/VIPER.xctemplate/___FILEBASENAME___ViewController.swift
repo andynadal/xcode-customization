af0706528ddd51167fae60b4688805a27e8a89c6
Eugenio Leal <eugenioleal333@gmail.com>
2022-11-01 13:19:46 -0600

    [PROTOCOL] inherit the View protocol in View controller class

--- templates/interface/VIPER.xctemplate/___FILEBASENAME___ViewController.swift
+++ templates/interface/VIPER.xctemplate/___FILEBASENAME___ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-final class ___VARIABLE_productName:identifier___ViewController: UIViewController {
+final class ___VARIABLE_productName:identifier___ViewController: UIViewController, ___VARIABLE_productName:identifier___ViewProtocol {
 
     var presenter: ___VARIABLE_productName:identifier___PresenterProtocol?
 
